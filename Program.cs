75eed76454dfd1c4632e8d8305e7245991847212
laPemaKu <113007919+laPemaKu@users.noreply.github.com>
2023-09-14 20:40:39 +0200

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -10,6 +10,7 @@ namespace inicijalno_ponavljanje_1
     {
         static void Main(string[] args)
         {
+            //Znam da u zadatku pise da trebamo unjeti broj brojeva u listi ali mi je izgledalo kao ne potreban dio pošto liste ne trebaju definirani broj
             Console.WriteLine("Upišite niz brojeva, nakn što ste gotovi upišite, 'gotovo'");
             string answer = Console.ReadLine();
             List <double> list = new List<double>();
