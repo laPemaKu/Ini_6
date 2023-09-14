056c56b14765fa7aa039e9fdfff19b2d8fe8c52e
laPemaKu <113007919+laPemaKu@users.noreply.github.com>
2023-09-14 20:37:29 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace inicijalno_ponavljanje_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Upišite niz brojeva, nakn što ste gotovi upišite, 'gotovo'");
+            string answer = Console.ReadLine();
+            List <double> list = new List<double>();
+            while (answer != "gotovo")
+            {
+                list.Add(Convert.ToDouble(answer));
+                answer = Console.ReadLine();
+            }
+            double zbroj= 0;
+            foreach (double number in list)
+            {
+                zbroj = zbroj + number;
+            }
+
+            Console.WriteLine("Summa brojeva sa liste je " + zbroj);
+            Console.ReadKey();
+        }
+    }
+}
